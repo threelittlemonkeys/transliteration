17dea103814882a12a8580a70754bcd0586ae1c0
threelittlemonkeys <sjlee7642@gmail.com>
2023-09-26 11:21:31 +0900

    Update by commit

--- data/detect_tw.py
+++ data/detect_tw.py
@@ -1,6 +1,7 @@
 import sys
 
 cn = set()
+
 with open("cntw.mono.tsv") as fo:
     for line in fo:
         s, *t = line.strip().split("\t")
@@ -9,6 +10,8 @@ with open("cntw.mono.tsv") as fo:
         cn.add(s)
 
 for line in sys.stdin:
+
     if cn.intersection(line):
         continue
+
     print(line, end = "")
