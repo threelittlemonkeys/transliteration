e118d043404cf33353300982cb3b458a4d8e81ff
threelittlemonkeys <sjlee7642@gmail.com>
2024-02-22 13:28:30 +0900

    Update by commit

--- scripts/enko.py
+++ scripts/enko.py
@@ -74,7 +74,7 @@ def syllabify_en(en, _ipa):
 
     # vowels
     V1 = "[aeiou]|y(?![aeou])"
-    V2 = "[aeo][ouw]|[aeou][iy]|[eo]a|[eiou]e"
+    V2 = "[aeo]a|[eiou]e|[aeo][ouw]|[aeou][iy]"
     V3 = "(au|e?i)gh|(?<=[st])io(?=n)"
 
     # phoneme segmentation
@@ -83,6 +83,7 @@ def syllabify_en(en, _ipa):
 
     # onset maximalization
     _en = re.sub(f" ?({C3}|{C2}|{C1}) _", r" \1_", _en)
+    _en = re.sub("^([^ ]+) ", r"\1", _en)
 
     # coda concatenation
     _en = re.sub("(_[^ ˈˌ]+)(( [^ _]+)+)(?=[ ˈˌ])", concat_coda, _en)
@@ -99,10 +100,21 @@ def syllabify_en(en, _ipa):
 
     return _en
 
-def preproc_enko(_en, _ipa):
+def syllabify_enko(_en, _ipa):
+
+    _en = [[x for x in xs] for xs in _en]
+    _ipa = [[x for x in xs] for xs in _ipa]
+
+    _en = syllabify_enko_phase1(_en, _ipa)
+    _ko = syllabify_enko_phase2(_en, _ipa)
+    ko = syllabify_enko_phase3(_en, _ko)
+
+    return ko
+
+def syllabify_enko_phase1(_en, _ipa):
 
     if len(_ipa) == 1:
-        return en
+        return _en
 
     if len(_en) != len(_ipa):
         return _en
@@ -115,19 +127,25 @@ def preproc_enko(_en, _ipa):
         if len(a) < 2 or len(b) < 2:
             continue
 
-        if a[1] == "i" and b[1] == "ə" and b[2][:1] != "r":
+        if b[2] and b[2][0] == "r":
+            continue
+
+        if a[1] == "i" and b[1] == "ə": # reduced vowel i
             b[1] = "i"
+
+        elif re.match("[bcdfgkpstz]l", a[0]) and a[1] == "e" \
+        and len(b[0]) == 1 and b[1] == "ə" and b[2][0] == "l":
+            b[1] = [""]
 
     return _en
 
-def syllabify_enko(_en, _ipa):
+def syllabify_enko_phase2(_en, _ipa):
 
     # consonants
     C1 = "dʒ|tʃ|[bdfghjklmnprstvwzðŋʃʒθ]"
     C2 = "[bdfghklmnpstvzʒθ]j|dʒj|[gk]w"
     C3 = "dz|ts|l[mn]|rl"
 
-    _en = preproc_enko(_en, _ipa)
     _ko = []
 
     for i, x in enumerate(_ipa):
@@ -135,7 +153,7 @@ def syllabify_enko(_en, _ipa):
         try:
             o, n, c = x
         except: # invalid syllable
-            return None
+            return ""
 
         y = []
 
@@ -149,12 +167,18 @@ def syllabify_enko(_en, _ipa):
         for p in o:
 
             # syllable-initial /l/
-            if p == "l":
-                s = y[-1] if y else _ko[-1][-1] if _ko else None # last syllable
-                if s and len(s) != 3:
+            if p == "l" and (y or _ko):
+                s = y[-1] if y else _ko[-1][-1]
+                if len(s) != 3:
                     if len(s) == 1:
                         s.append("")
                     s.append("l")
+
+            # syllable-initial /m/, /n/
+            if p in ("m", "n") and _ko:
+                s = _ko[-1]
+                if len(s[-1]) == 3 and s[-1][2] in ("k", "p"):
+                    s.append([s[-1].pop()])
 
             y.append([p])
 
@@ -177,7 +201,16 @@ def syllabify_enko(_en, _ipa):
 
             # coda already occupied
             if len(y[-1]) != 2:
-                y.append([])
+
+                # syllable-final /l/, /m/, /n/
+                if p in ("l", "m", "n"):
+                    if len(y[-1]) == 1:
+                        y[-1].append("")
+                    elif y[-1][-1] == "":
+                        y[-1].pop()
+
+                else:
+                    y.append([])
 
             # syllable-final /lm/, /ln/
             elif p in ("lm", "ln"):
@@ -187,8 +220,8 @@ def syllabify_enko(_en, _ipa):
 
             # syllable-final /r/
             elif p == "r":
-                if y[-1][-1] == "e": # air /ɛɹ/ -> /ɛəɹ/
-                    y.append(["", "ə"])
+                if y[-1][-1][-1] in ("e", "i"): # /ɛɹ/, /ɪɹ/
+                    y.append(["", "ə"]) # /ɛə/, /ɪə/
                 p = ""
 
             # syllable-final /rl/
@@ -203,11 +236,9 @@ def syllabify_enko(_en, _ipa):
 
         _ko.append(y)
 
-    ko = ipa_to_hangeul(_en, _ko)
+    return _ko
 
-    return ko
-
-def ipa_to_hangeul(_en, _ko) : # IPA to Hangeul syllables
+def syllabify_enko_phase3(_en, _ko) : # IPA to Hangeul syllables
 
     _ENKO = {
         **{a: b for a, b in zip(
@@ -240,6 +271,8 @@ def ipa_to_hangeul(_en, _ko) : # IPA to Hangeul syllables
                 x[0] = "ㅇ"
             if x[1] == "":
                 x[1] = "ㅣ" if x[0][-1] in ("ʃ", "ʒ") else "ㅡ"
+            if x[1] == " ":
+                x[1] = "ㅡ"
 
             x = [_ENKO.get(p, p) for p in x]
 
@@ -265,6 +298,9 @@ if __name__ == "__main__":
         _ipa = syllabify_ipa(ipa)
         _en = syllabify_en(en, _ipa)
         ko = syllabify_enko(_en, _ipa)
+
+        # print(len(_ipa), _ipa)
+        # print(len(_en), _en)
 
         if ko == None:
             print(en, ipa, "", "", "", sep = "\t")
